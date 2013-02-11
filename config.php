4fff7333b38bf59f75bb22d6f68087a888614164
Sean <kana@Ireland.(none)>
2013-02-11 01:27:26 -0600

    Altering the plugin name to make it dynamic

--- config.php
+++ config.php
@@ -4,7 +4,7 @@ DEFINE('WPC_DB', $wpdb->prefix . "wpc_events");
 DEFINE('WPC_EVENT_TYPE_DB', $wpdb->prefix . "wpc_types");
 
 DEFINE('WPC_VERSION', '1.3.4');
-DEFINE('WPC_PLUGIN_NAME', "wordpress-calendar");
+DEFINE('WPC_PLUGIN_NAME', basename(dirname( __FILE__ )));
 DEFINE('WPC_DISPLAY_NAME', "WP Calendar");
 
 //require_once(WPC_PATH . 'functions.php');
